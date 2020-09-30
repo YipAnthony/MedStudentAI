f7ecd5e72029d6d8d00570a3e34cb8ca526d8c4e
Anthony Yip <anthonyjtyip@gmail.com>
2020-09-29 20:33:11 -0700

    Add fuzzyset backup search for misspelled words

--- src/components/Gender.js
+++ src/components/Gender.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 export default function Gender(props) {
+   
     return (
-        
         <div>
             Gender: 
-            <button id="inputGender" className="btn btn-outline-primary btn-sm m-1" onClick={props.handleChange}>Male</button>
-            <button id="inputGender" className="btn btn-outline-primary btn-sm m-1" onClick={props.handleChange}>Female</button>
+            <button 
+                id="inputGender" 
+                className={props.selectedGender === "Male" ? "btn btn-outline-primary btn-sm m-1 active shadow-none": "btn btn-outline-primary btn-sm m-1"}
+                onClick={props.handleChange}>
+                    Male
+            </button>
+            <button 
+                id="inputGender" 
+                className={props.selectedGender === "Female" ? "btn btn-outline-primary btn-sm m-1 active shadow-none": "btn btn-outline-primary btn-sm m-1"}
+                onClick={props.handleChange}>
+                    Female
+            </button>
         </div>
     )
 }
+
