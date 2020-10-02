c6ddaf506b7fb063419aedace9f3ca850bf3fb40
Anthony Yip <anthonyjtyip@gmail.com>
2020-10-02 09:10:59 -0700

    Add toggle tab (mount/unmount) for chief complain, additional symptoms, and labs

--- src/components/AddSymptom.js
+++ src/components/AddSymptom.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {toggleUpIcon, toggleDownIcon} from '../icons'
 
 export default function AddSymptom(props) {
 
+    let [showSymptom, setShowSymptom] = useState(false)
+
+    function toggle() {
+        setShowSymptom(prev => !prev)
+    }
     return (
-        <div className="dropdown d-flex mt-3">
-            
-            <textarea 
-                className="form-control shadow-none" 
-                type="textarea" 
-                rows="3"
-                id="patientAdditionalSymptom" 
-                onChange={props.handleChange} 
-                placeholder={"Additional Symptoms: e.g. patient endorses chest pain for 1 week, worsened with exercise, and difficulty breathing" }
-                autoComplete="off"
-                value={props.selectedSymptomsInput} 
-            ></textarea>
-            <button 
-                className="btn btn-sm btn-primary shadow-none"
-                onClick={props.handleSearch}
-            >   Search
-            </button>
+        <div className="card">
+            <h5 
+                className='card-header toggleTab'
+                onClick={toggle}
+            >
+                Additional Symptoms
+                {showSymptom ? toggleUpIcon: toggleDownIcon}
+            </h5>
+            {showSymptom &&
+                <div className="card-body">
+                    <div className="dropdown d-flex mt-3">
+                        <textarea 
+                            className="form-control shadow-none" 
+                            type="textarea" 
+                            rows="5"
+                            id="patientAdditionalSymptom" 
+                            onChange={props.handleChange} 
+                            placeholder={"Can interpret multiple phrases \nExample: patient endorses chest pain for 1 week, worsened with exercise, and difficulty breathing" }
+                            autoComplete="off"
+                            value={props.selectedSymptomsInput} 
+                        ></textarea>
+                        <button 
+                            className="btn btn-sm btn-primary shadow-none"
+                            onClick={props.handleSearch}
+                        >   Search
+                        </button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/components/ChiefComplaint.js
+++ src/components/ChiefComplaint.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {toggleUpIcon, toggleDownIcon} from '../icons'
 
 export default function Symptoms(props) {
 
+    let [showCC, setShowCC] = useState(false)
+
+    function toggle() {
+        setShowCC(prev => !prev)
+    }
 
     return (
-        <div className="d-flex">
-            <input 
-                className="form-control" 
-                type="text" 
-                id="patientCC" 
-                onChange={props.handleChange} 
-                placeholder={"Chief complaint"}
-                autoComplete="off"
-                value={props.ChiefComplaintInput} 
-            ></input>
-             <button 
-                    id = "searchCCButton"
-                    className="btn btn-sm btn-primary shadow-none"
-                    data-array = {props.arrayNumber}
-                    onClick={props.handleSearch}
-                >Search</button>
+        <div className="card">
+            <h5 
+                className='card-header toggleTab'
+                onClick={toggle}
+            >
+                Chief complaint
+                {showCC ? toggleUpIcon: toggleDownIcon}
+            </h5>
+            {showCC &&
+                <div className="card-body">
+                    <div className="d-flex">
+                        <input 
+                            className="form-control" 
+                            type="text" 
+                            id="patientCC" 
+                            onChange={props.handleChange} 
+                            placeholder={"e.g. syncope, dyspnea, chest pain..."}
+                            autoComplete="off"
+                            value={props.ChiefComplaintInput} 
+                        ></input>
+                        <button 
+                                id = "searchCCButton"
+                                className="btn btn-sm btn-primary shadow-none"
+                                data-array = {props.arrayNumber}
+                                onClick={props.handleSearch}
+                            >Search</button>
+                    </div>
+                </div>
+            }
          </div>
     )
 }
