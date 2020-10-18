dc6b4f83f25613191b02e0fbd4fa6ba4f9c9c509
Anthony Yip <anthonyjtyip@gmail.com>
2020-10-18 08:22:26 -0700

    Updates

---
+++ precache-manifest.7b61f8011a15d2573ad32b1b3c01ab75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec4e53df1766e31cee57c012ddc7da5",
+    "url": "/MedStudentAI/index.html"
+  },
+  {
+    "revision": "5e4087093b07ea991aa6",
+    "url": "/MedStudentAI/static/css/main.c9cfc826.chunk.css"
+  },
+  {
+    "revision": "1c63e4742dce689e3c1e",
+    "url": "/MedStudentAI/static/js/2.8020ba0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MedStudentAI/static/js/2.8020ba0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4087093b07ea991aa6",
+    "url": "/MedStudentAI/static/js/main.a7fb08b3.chunk.js"
+  },
+  {
+    "revision": "eb231bd9d7167b5f7c20",
+    "url": "/MedStudentAI/static/js/runtime-main.89cafb08.js"
+  }
+]);
